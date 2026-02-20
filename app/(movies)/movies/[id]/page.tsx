c03b26a595adb0a32e4d7d15ddb4920cc54c86e2
JongminChoi98 <joeychoi7624@gmail.com>
2026-02-20 10:07:56 +0900

    Next Data Fetching

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,22 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
 export default async function MovieDetail({
   params,
-  searchParams,
 }: {
   params: Promise<{ id: string }>;
-  searchParams: Promise<Record<string, string>>;
 }) {
   const { id } = await params;
-  const query = await searchParams;
-
-  console.log("searchParams:", query);
-
-  return <h1>Movie Detail: {id}</h1>;
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
